4686643d8b57184158ac644fefe3eec3c01f58ce
Agustin <agumatthey@gmail.com>
2021-12-16 13:17:11 -0300

    se agrega parte de transformaciones a la creacion de la escena para el ejemplo IK

--- MonaEngine/examples/ExampleFABRIK.cpp
+++ MonaEngine/examples/ExampleFABRIK.cpp
@@ -1,40 +1,8 @@
 #include "MonaEngine.hpp"
 #include "Rendering/DiffuseFlatMaterial.hpp"
 
-void CreateBasicCameraWithMusicAndLight(Mona::World& world)
-{
-	auto camera = world.CreateGameObject<Mona::GameObject>();
-	auto transform = world.AddComponent<Mona::TransformComponent>(camera, glm::vec3(0.0f, -15.0f, 15.0f));
-	transform->Rotate(glm::vec3(-1.0f, 0.0f, 0.0f), 0.5f);
-	world.SetMainCamera(world.AddComponent<Mona::CameraComponent>(camera));
-	world.SetAudioListenerTransform(transform);
-	auto& audioClipManager = Mona::AudioClipManager::GetInstance();
-	auto audioClipPtr = audioClipManager.LoadAudioClip(Mona::SourcePath("Assets/AudioFiles/music.wav"));
-	auto audioSource = world.AddComponent<Mona::AudioSourceComponent>(camera, audioClipPtr);
-	audioSource->SetIsLooping(true);
-	audioSource->SetVolume(0.3f);
-	audioSource->Play();
 
-	world.AddComponent<Mona::DirectionalLightComponent>(camera, glm::vec3(1.0f));
-
-}
-
-void CreateWall(Mona::World& world,
-	const glm::vec3& position,
-	const glm::vec3& scale,
-	std::shared_ptr<Mona::Material> wallMaterial) {
-	auto& meshManager = Mona::MeshManager::GetInstance();
-	auto wall = world.CreateGameObject<Mona::GameObject>();
-	world.AddComponent<Mona::TransformComponent>(wall, position, glm::fquat(1.0f, 0.0f, 0.0f, 0.0f), scale);
-	world.AddComponent<Mona::StaticMeshComponent>(wall, meshManager.LoadMesh(Mona::Mesh::PrimitiveType::Cube), wallMaterial);
-	Mona::BoxShapeInformation wallShape(scale);
-	Mona::RigidBodyHandle rb = world.AddComponent<Mona::RigidBodyComponent>(wall, wallShape, Mona::RigidBodyType::StaticBody);
-	rb->SetRestitution(1.0f);
-	rb->SetFriction(0.0f);
-}
-
-
-class animatedChain :public Mona::GameObject {
+class AnimatedChain :public Mona::GameObject {
 
 private:
 	void SetTargetPosition(const glm::vec3 position) {}
@@ -56,31 +24,32 @@ public:
 		m_transform = world.AddComponent<Mona::TransformComponent>(*this);
 		// creamos la escena que contiene la cadena
 		aiScene scene = aiScene();
-		std::vector<aiMesh> meshes = {};
+		std::vector<aiMesh> meshes = {}; // meshes para agregar a la escena
 		int numOfSegments = 10;
 		unsigned int i = 0;
 		// create vector with nodes. nombramos nodos y huesos correspondientes con indice de asignacion
-		std::vector<aiNode> nodes = {};
+		std::vector<aiNode*> nodes = {};
 		// hay una malla por cada nodo (esfera(joint) o cubo(link))
-		nodes[i].mMeshes = {i, i+1};
-		nodes[i].mNumMeshes;
-		nodes[i].mParent;
-		nodes[i].mTransformation;
-		// el nodo 0 será el nodo raiz de la escena.
-		for (i = 0; i < numOfSegments; i++) {
+		// el nodo 0_joint será el nodo raiz de la escena.
+		bool configEndEffector = false;
+		for (i = 0; i <= numOfSegments; i++) {
+			if (i == numOfSegments) {
+				configEndEffector = true;
+			}
 			std::string jointName = std::to_string(i) + "_joint";
+			if (configEndEffector) {
+				std::string jointName = "endEffector";
+			}			
 			// create joint node
-			aiNode jointNode = aiNode(jointName);
+			aiNode jointNode = &aiNode(jointName);
 			nodes.push_back(jointNode);
-			nodes[2*i].mMeshes = { 2*i };
-			nodes[2*i].mNumMeshes = 1;
-			nodes[2*i].mTransformation;
+			nodes[2*i]->mMeshes = { 2*i };
+			nodes[2*i]->mNumMeshes = 1;
 			// create joint mesh
 			aiMesh* sphereMesh = Mona::Mesh::sphereMeshData();
 			meshes.push_back(*sphereMesh);
-			meshes[2*i].mNumFaces;
 			// add bone
-			aiBone jointBone = aiBone(jointName);
+			aiBone* jointBone = &aiBone(jointName);
 			std::vector<unsigned int> sphereIndices = {};
 			for (int i = 0; i < sphereMesh->mNumFaces; i++) {
 				aiFace f = sphereMesh->mFaces[i];
@@ -91,64 +60,140 @@ public:
 			// remove duplicated indices
 			std::sort(sphereIndices.begin(), sphereIndices.end());
 			sphereIndices.erase(std::unique(sphereIndices.begin(), sphereIndices.end()), sphereIndices.end());
-			jointBone.mNumWeights = sphereIndices.size();
+			jointBone->mNumWeights = sphereIndices.size();
 			std::vector<aiVertexWeight> jointWeights = {};
-			for (int i = 0; sphereIndices.size(); i++) {
+			for (int i = 0; jointBone->mNumWeights; i++) {
 				aiVertexWeight w = aiVertexWeight();
 				w.mVertexId = sphereIndices[i];
 				w.mWeight = 1.0f;
 				jointWeights.push_back(w);
 			}
-			jointBone.mOffsetMatrix;
-			meshes[2*i].mBones;
+			jointBone->mWeights = &jointWeights[0];
+			jointBone->mOffsetMatrix;
+			meshes[2 * i].mBones = jointBone;
 
-			// create link node
-			std::string linkName = std::to_string(i) + "_link";
-			aiNode linkNode = aiNode(linkName);
-			nodes.push_back(linkNode);
-			nodes[2*i + 1].mMeshes = { 2*i + 1 };
-			nodes[2*i + 1].mNumMeshes = 1;
-			nodes[2*i + 1].mTransformation;
-			// create link mesh
-			aiMesh* cubeMesh = Mona::Mesh::cubeMeshData();
-			meshes.push_back(*cubeMesh)
-			meshes[2*i + 1].mNumFaces;
-			// add bone
-			aiBone linkBone = aiBone(linkName);
-			std::vector<unsigned int> cubeIndices = {};
-			for (int i = 0; i < cubeMesh->mNumFaces; i++) {
-				aiFace f = cubeMesh->mFaces[i];
-				cubeIndices.push_back(f.mIndices[0]);
-				cubeIndices.push_back(f.mIndices[1]);
-				cubeIndices.push_back(f.mIndices[2]);
+			if (!configEndEffector) { // si llegamos al endEffector, no hace falta agregar un link luego.
+				// create link node
+				std::string linkName = std::to_string(i) + "_link";
+				aiNode* linkNode = &aiNode(linkName);
+				nodes.push_back(linkNode);
+				nodes[2 * i + 1]->mMeshes = { 2 * i + 1 };
+				nodes[2 * i + 1]->mNumMeshes = 1;
+				// create link mesh
+				aiMesh* cubeMesh = Mona::Mesh::cubeMeshData();
+				meshes.push_back(*cubeMesh);
+				// add bone
+				aiBone* linkBone = &aiBone(linkName);
+				std::vector<unsigned int> cubeIndices = {};
+				for (int i = 0; i < cubeMesh->mNumFaces; i++) {
+					aiFace f = cubeMesh->mFaces[i];
+					cubeIndices.push_back(f.mIndices[0]);
+					cubeIndices.push_back(f.mIndices[1]);
+					cubeIndices.push_back(f.mIndices[2]);
+				}
+				// remove duplicated indices
+				std::sort(cubeIndices.begin(), cubeIndices.end());
+				cubeIndices.erase(std::unique(cubeIndices.begin(), cubeIndices.end()), cubeIndices.end());
+				linkBone->mNumWeights = cubeIndices.size();
+				std::vector<aiVertexWeight> linkWeights = {};
+				for (int i = 0; linkBone->mNumWeights; i++) {
+					aiVertexWeight w = aiVertexWeight();
+					w.mVertexId = cubeIndices[i];
+					w.mWeight = 1.0f;
+					linkWeights.push_back(w);
+				}
+				linkBone->mWeights = &linkWeights[0];
+				linkBone->mOffsetMatrix;
+				meshes[2 * i + 1].mBones = linkBone;
 			}
-			// remove duplicated indices
-			std::sort(cubeIndices.begin(), cubeIndices.end());
-			cubeIndices.erase(std::unique(cubeIndices.begin(), cubeIndices.end()), cubeIndices.end());
-			linkBone.mNumWeights = cubeIndices.size();
-			std::vector<aiVertexWeight> linkWeights = {};
-			for (int i = 0; cubeIndices.size(); i++) {
-				aiVertexWeight w = aiVertexWeight();
-				w.mVertexId = cubeIndices[i];
-				w.mWeight = 1.0f;
-				linkWeights.push_back(w);
-			}
-			linkBone.mOffsetMatrix;
-			meshes[2*i + 1].mBones;
-
 		}
-		// por último se crea el nodo para el end-effector
 		
-		// falta asignar relaciones padre-hijo
-		for (i = 1; i < numOfSegments; i++) {
-			nodes.push_back(aiNode());
-			nodes[i].mChildren;
-			nodes[i].mNumChildren;
-			nodes[i].mMeshes;
-			nodes[i].mNumMeshes;
-			nodes[i].mParent;
-			nodes[i].mTransformation;
+		// asignamos relaciones padre-hijo
+		scene.mRootNode = nodes[0];
+		nodes[0]->mChildren = &nodes[1];
+		nodes[i]->mNumChildren = 1;
+		nodes[i]->mParent = nullptr;
+		for (i = 1; i < 2*numOfSegments; i++) {
+			nodes[i]->mChildren = &nodes[i + 1];
+			nodes[i]->mNumChildren = 1;
+			nodes[i]->mParent = nodes[i - 1];
 		}
+		// para el end-effector
+		nodes[2 * numOfSegments]->mChildren = nullptr;
+		nodes[2 * numOfSegments]->mNumChildren = 0;
+		nodes[2 * numOfSegments]->mParent = nodes[2 * numOfSegments - 1];
+
+		// por ultimo seteamos las transformaciones
+		// los dos primeros nodos son especiales
+		float bScale = 1.0f; // base scale
+		float flLen = 6.0f; // first link relative length
+		float flThin = 0.8f; // make first link thinner
+		float scaleDown = 0.8f; // reduce size of joints and links further in the chain
+		float linkCurrentLength = bScale * flLen;
+		aiVector3D scaling = aiVector3D(bScale, bScale, bScale); // escalamiento base
+		aiQuaternion rotation = aiQuaternion(0.0f, 0.0f, 0.0f);
+		aiVector3D translation = aiVector3D(0.0f, 0.0f, 0.0f);
+		aiMatrix4x4 jointTransform = aiMatrix4x4(scaling, rotation, translation);
+		aiMatrix4x4 finalJointTransform = jointTransform;
+		nodes[0]->mTransformation = finalJointTransform;
+		// link
+		scaling = aiVector3D(flThin, flLen, flThin); // escalamiento base
+		rotation = aiQuaternion(0.0f, 0.0f, 0.0f);
+		translation = aiVector3D(0.0f, (bScale * flLen) / 2, 0.0f); // dejamos la base del primer link en (0,0,0)
+		aiMatrix4x4 linkTransform = aiMatrix4x4(scaling, rotation, translation);
+		aiMatrix4x4 finalLinkTransform = linkTransform;
+		nodes[1]->mTransformation = finalLinkTransform;
+		
+		// acc transform
+		aiMatrix4x4 accTransform = finalLinkTransform * finalJointTransform;
+		// acc joint transform
+		aiMatrix4x4 accJointTransform = jointTransform;
+		// acc link transform
+		aiMatrix4x4 accLinkTransform = linkTransform;
+		// joint 
+
+		configEndEffector = false;
+		for (int i = 1; i <= numOfSegments) {
+			if (i == numOfSegments) {
+				configEndEffector = true;
+				scaleDown = 1.0f;
+			}
+			// joint
+			scaling = aiVector3D(scaleDown, scaleDown, scaleDown);
+			rotation = aiQuaternion(0.0f, 0.0f, 0.0f);
+			translation = aiVector3D(0.0f, linkCurrentLength, 0.0f);
+			jointTransform = aiMatrix4x4(scaling, rotation, translation);
+			finalJointTransform = jointTransform * accJointTransform * aiMatrix4x4::Inverse(accTransform);
+			nodes[2 * i]->mTransformation = finalJointTransform;
+
+			if (!configEndEffector) {
+				// acc transform
+				aiMatrix4x4 accTransform = finalJointTransform * accTransform;
+				// acc joint transform
+				aiMatrix4x4 accJointTransform = jointTransform * accJointTransform;
+
+				// link
+				scaling = aiVector3D(scaleDown, scaleDown, scaleDown);
+				rotation = aiQuaternion(0.0f, 0.0f, 0.0f);
+				translation = aiVector3D(0.0f, linkCurrentLength, 0.0f);
+				linkTransform = aiMatrix4x4(scaling, rotation, translation);
+				finalLinkTransform = linkTransform * accLinkTransform * aiMatrix4x4::Inverse(accTransform);
+				nodes[2 * i + 1]->mTransformation = finalLinkTransform;
+
+				// acc transform
+				aiMatrix4x4 accTransform = finalLinkTransform * accTransform;
+				// acc link transform
+				aiMatrix4x4 accLinkTransform = linkTransform * accLinkTransform;
+
+				linkCurrentLength = linkCurrentLength * scaleDown;
+			}
+			
+		}
+
+		//para el endeffector
+
+
+
 		// assign root node
 		// create mesh
 		// create base animation
@@ -156,15 +201,6 @@ public:
 	}
 
 };
-void CreateAnimatedChain(Mona::World& world, int numOfSegments) {
-	auto chain = world.CreateGameObject<Mona::GameObject>();
-	for (int i; i < numOfSegments; i++) {
-
-
-	}
-
-
-}
 
 class Character : public Mona::GameObject
 {
